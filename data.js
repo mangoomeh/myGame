07a75f1cd771291e119ed02cd2f3b702e7186a34
Rui Bing <rbruibingnet@gmail.com>
2021-11-23 14:18:00 +0800

    moved data into data.js

---
+++ data.js
@@ -0,0 +1,156 @@
+// datafile
+const titleDeeds = [
+    {
+      colour: "BROWN",
+      title: "OLD KENT ROAD",
+      price: 60,
+      rent: 2,
+    },
+  
+    {
+      colour: "BROWN",
+      title: "WHITECHAPEL ROAD",
+      price: 60,
+      rent: 4,
+    },
+  
+    {
+      colour: "LIGHTBLUE",
+      title: "THE ANGEL ISLINGTON ROAD",
+      price: 100,
+      rent: 6,
+    },
+  
+    {
+      colour: "LIGHTBLUE",
+      title: "EUSTON ROAD",
+      price: 100,
+      rent: 6,
+    },
+  
+    {
+      colour: "LIGHTBLUE",
+      title: "PENTONVILLE ROAD",
+      price: 120,
+      rent: 8,
+    },
+  
+    {
+      colour: "PINK",
+      title: "PALL MALL",
+      price: 140,
+      rent: 10,
+    },
+  
+    {
+      colour: "PINK",
+      title: "WHITEHALL",
+      price: 140,
+      rent: 10,
+    },
+  
+    {
+      colour: "PINK",
+      title: "NORTHUMBERLAND AVENUE",
+      price: 160,
+      rent: 12,
+    },
+  
+    {
+      colour: "ORANGE",
+      title: "BOW STREET",
+      price: 180,
+      rent: 14,
+    },
+  
+    {
+      colour: "ORANGE",
+      title: "MARLBOROUGH STREET",
+      price: 180,
+      rent: 14,
+    },
+  
+    {
+      colour: "ORANGE",
+      title: "VINE STREET",
+      price: 200,
+      rent: 16,
+    },
+  
+    {
+      colour: "RED",
+      title: "STRAND",
+      price: 220,
+      rent: 18,
+    },
+  
+    {
+      colour: "RED",
+      title: "FLEET STREET",
+      price: 220,
+      rent: 18,
+    },
+  
+    {
+      colour: "RED",
+      title: "TRAFALGAR SQUARE",
+      price: 240,
+      rent: 20,
+    },
+  
+    {
+      colour: "YELLOW",
+      title: "LEICESTER SQUARE",
+      price: 260,
+      rent: 22,
+    },
+  
+    {
+      colour: "YELLOW",
+      title: "COVENTRY STREET",
+      price: 260,
+      rent: 22,
+    },
+  
+    {
+      colour: "YELLOW",
+      title: "PICCADILLY",
+      price: 280,
+      rent: 24,
+    },
+  
+    {
+      colour: "GREEN",
+      title: "REGENT STREET",
+      price: 300,
+      rent: 26,
+    },
+  
+    {
+      colour: "GREEN",
+      title: "OXFORD STREET",
+      price: 300,
+      rent: 26,
+    },
+  
+    {
+      colour: "GREEN",
+      title: "BOND STREET",
+      price: 320,
+      rent: 28,
+    },
+  
+    {
+      colour: "BLUE",
+      title: "PARK LANE",
+      price: 350,
+      rent: 35,
+    },
+  
+    {
+      colour: "BLUE",
+      title: "MAYFAIR",
+      price: 400,
+      rent: 50,
+    },
+  ];
